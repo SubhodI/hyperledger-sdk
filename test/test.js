610f408426c31f7c5e8075514874e77552d3e149
SubhodI <subhodpoojary@gmail.com>
2017-07-24 18:47:01 +0530

    Added modules

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
 var sdk= require("../index.js");
 helper=sdk.helper;
 helper.setChain("/root/hyperledger/hyperledger-sdk/test/network-config.json","/root/hyperledger/hyperledger-sdk/test/config.json");
-console.log(helper.getClientForOrg('org3'));
+console.log(sdk);
