256ce4a76f5627d1fd1f040570284f37eb1c4a60
Siddesh Sangodkar <siddesh226@gmail.com>
2017-07-25 17:40:00 +0530

    added channel functions

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var helper = require('./lib/helper.js');
 var chaincode = require('./lib/chaincode-opeartions.js');
 var user = require('./lib/user-management.js');
+var channel = require('./lib/channel.js');
+
 module.exports = {
     helper: helper,
     chaincode: chaincode,
-    user: user
+    user: user,
+	channel:channel
 }
