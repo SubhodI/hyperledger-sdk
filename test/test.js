bb5dd4fc1a315e44b96d3be83493bd9a482b52b4
SubhodI <subhodpoojary@gmail.com>
2017-07-25 16:46:52 +0530

    Chaincode operation and helper ORGS added

--- test/test.js
+++ test/test.js
@@ -1,4 +1,7 @@
 var sdk= require("../index.js");
 helper=sdk.helper;
 helper.setChain("/root/hyperledger/hyperledger-sdk/test/network-config.json","/root/hyperledger/hyperledger-sdk/test/config.json");
-console.log(sdk);
+console.log(helper.getORGS());
+// setTimeout(function() {
+//     console.log(sdk);    
+// }, 9000);
