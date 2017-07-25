bc64972b157820a162517eb4d68f6c3a533965e6
SubhodI <subhodpoojary@gmail.com>
2017-07-25 18:06:09 +0530

    create and join channel added

--- test/test.js
+++ test/test.js
@@ -1,7 +1,14 @@
 var sdk= require("../index.js");
 helper=sdk.helper;
+channel=sdk.channel;
+
 helper.setChain("/root/hyperledger/hyperledger-sdk/test/network-config.json","/root/hyperledger/hyperledger-sdk/test/config.json");
-console.log(helper.getORGS());
-// setTimeout(function() {
-//     console.log(sdk);    
-// }, 9000);
+
+//console.log(helper.getORGS());
+
+//channel.createChannel("firstchannel","/root/hyperledger/hyperledger-sdk/test/channel-artifacts/channel.tx","admin","org1");
+
+// channel.joinChannel("firstchannel", ["localhost:7051","localhost:8051"], "admin", "org1")
+// 	.then(function(message) {
+// 		console.log(message);
+// 	});
