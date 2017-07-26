9da91e8a2c40b0939010b618ddd128476f448610
Siddesh Sangodkar <siddesh226@gmail.com>
2017-07-26 16:20:11 +0530

    updated helper.js config GOpath, tests

--- test/test_user_management.js
+++ test/test_user_management.js
@@ -2,10 +2,38 @@ var sdk= require("../index.js");
 helper=sdk.helper;
 helper.setChain("/root/workspace/hyperledger/hyperledger-sdk/test/network-config.json","/root/workspace/hyperledger/hyperledger-sdk/test/config.json");
 console.log(sdk);
-
+user=sdk.user;
 console.log(sdk.user);
 
 
+
+// Register new User
+// sdk.helper.getRegisteredUsers("Jim", 'org1', true).then(function(response) {
+		// if (response && typeof response !== 'string') {
+			// console.log(response);
+		// } else {
+			// console.log(response);
+		// }
+	// });
+
+	
+// install chaincode
+		
+	sdk.chaincode.installChaincode(["localhost:7051","localhost:8051"], 'mycc',"example_cc.go", 'v0', "Jim", 'org1')
+	.then(function(message) {
+		console.log(message);
+	});
+	
+	
+
+	
+	
+	
+	
+// user.query.getBlockByNumber("peer1", 1, "Jim", 'org1')
+		// .then(function(message) {
+			// console.log(message)
+		// });
 
 
 
