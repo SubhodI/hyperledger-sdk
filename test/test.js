0822ed1a6154fe1a76511c88c0c84153f216b70c
SubhodI <subhodpoojary@gmail.com>
2017-07-26 20:34:56 +0530

    GOPATH and deplpoy,instantiateChaincode test failing

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var sdk= require("../index.js");
 helper=sdk.helper;
 channel=sdk.channel;
-
+chaincode=sdk.chaincode;
 helper.setChain("/root/hyperledger/hyperledger-sdk/test/network-config.json","/root/hyperledger/hyperledger-sdk/test/config.json");
 
 //console.log(helper.getORGS());
@@ -12,3 +12,14 @@ helper.setChain("/root/hyperledger/hyperledger-sdk/test/network-config.json","/r
 // 	.then(function(message) {
 // 		console.log(message);
 // 	});
+
+
+//chaincode.installChaincode(["localhost:7051","localhost:8051"], "helloworld", "example_cc.go","v0", "admin", "org1");
+// file shoulld be inside config.GOPATH/src/example_cc.go
+
+//chaincode.instantiateChaincode("firstchannel", "helloworld", "v0", "init", ["a","100","b","200"],"admin", "org1");
+//error: chaincode name issue
+
+//invokeChaincode(["localhost:7051", "localhost:7056"], "mychannel", "mycc", "move", ["a","b","10"], "admin", "org1")
+//console.log(queryChaincode("peer1","mychannel","mycc", ['a'], "query", "admin", "org1"));
+//invokeChaincode(["localhost:7051", "localhost:7056"], "mychannel", "mycc", "move", ["a","b","10"], "admin", "org1")
