ce434aec3e246d2ab7738167128128df808b4ebe
Siddesh Sangodkar <siddesh226@gmail.com>
2017-07-28 17:59:45 +0530

    updated test file

--- test/test_user_management.js
+++ test/test_user_management.js
@@ -1,36 +1,54 @@
 var sdk= require("../index.js");
 helper=sdk.helper;
+
+// Set network-Config and sdk config path
 helper.setChain("/root/workspace/hyperledger/hyperledger-sdk/test/network-config.json","/root/workspace/hyperledger/hyperledger-sdk/test/config.json");
-console.log(sdk);
+
 user=sdk.user;
-console.log(sdk.user);
+// SDK 
+//console.log(sdk);
 
 
+	
+// Step 1: 
 
 // Register new User
-// sdk.helper.getRegisteredUsers("Jim", 'org1', true).then(function(response) {
-		// if (response && typeof response !== 'string') {
-			// console.log(response);
-		// } else {
-			// console.log(response);
-		// }
-	// });
-
-	
-// install chaincode
-		
-	sdk.chaincode.installChaincode(["localhost:7051","localhost:8051"], 'mycc',"example_cc.go", 'v0', "Jim", 'org1')
-	.then(function(message) {
-		console.log(message);
+sdk.helper.getRegisteredUsers("admin", 'org1', true).then(function(response) {
+		if (response && typeof response !== 'string') {
+			console.log(response);
+		} else {
+			console.log(response);
+		}
 	});
+
 	
+	
+// Step 2: 
+// Create channel "firstchannel" 
+
+//channel.createChannel("firstchannel","/root/workspace/hyperledger/hyperledger-sdk/test/channel-artifacts/channel.tx","admin","org1");
+
+//step 3:
+// join channel "firstchannel" 
+
+//channel.joinChannel("firstchannel", ["localhost:7051","localhost:8051"], "admin", "org1")
+	// .then(function(message) {
+		// console.log(message);
+	// });
+	
+//step 4:	
+// Install chaincode on peer
+		
+	// sdk.chaincode.installChaincode(["localhost:7051","localhost:8051"], 'mycc',"example_cc.go", 'v0', "admin", 'org1')
+	// .then(function(message) {
+		// console.log(message);
+	// });
 	
 
 	
 	
-	
-	
-// user.query.getBlockByNumber("peer1", 1, "Jim", 'org1')
+// Query
+// user.query.getBlockByNumber("peer2", 1, "admin", 'org1')
 		// .then(function(message) {
 			// console.log(message)
 		// });
