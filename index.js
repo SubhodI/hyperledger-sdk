7785c773da683937aa27005d088a8c4103945243
Siddesh Sangodkar <siddesh226@gmail.com>
2017-07-28 19:09:25 +0530

    added chain functions , updated usermanagement

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var helper = require('./lib/helper.js');
-var chaincode = require('./lib/chaincode-opeartions.js');
-var user = require('./lib/user-management.js');
+var chaincode = require('./lib/chaincode-operations.js');
+var chain = require('./lib/chain.js');
+var user = require('./lib/user_management.js');
 var channel = require('./lib/channel.js');
 
 module.exports = {
     helper: helper,
     chaincode: chaincode,
     user: user,
-	channel:channel
+	channel:channel,
+	chain:chain
 }
