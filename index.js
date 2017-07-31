6ffde8607780f7b347f7546848bd5bd4d86be9ce
SubhodI <subhodpoojary@gmail.com>
2017-07-31 17:26:33 +0530

    Revoke user certificate added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var helper = require('./lib/helper.js');
 var chaincode = require('./lib/chaincode-operations.js');
 var chain = require('./lib/chain.js');
-var user = require('./lib/user_management.js');
+var user = require('./lib/user-management.js');
 var channel = require('./lib/channel.js');
 
 module.exports = {
--- test/test.js
+++ test/test.js
@@ -1,32 +1,40 @@
-var sdk= require("../index.js");
-helper=sdk.helper;
-channel=sdk.channel;
-chaincode=sdk.chaincode;
-user= sdk.user;
-helper.setChain("/root/hyperledger/hyperledger-sdk/test/network-config.json","/root/hyperledger/hyperledger-sdk/test/config.json");
+var sdk = require("../index.js");
+helper = sdk.helper;
+channel = sdk.channel;
+chaincode = sdk.chaincode;
+user = sdk.user;
+chain = sdk.chain;
+
+helper.setChain("/root/hyperledger/hyperledger-sdk/test/network-config.json", "/root/hyperledger/hyperledger-sdk/test/config.json");
 
 
 // Channel API's
-        //channel.createChannel("mychannel","/root/hyperledger/hyperledger-sdk/test/artifacts/channel/mychannel.tx","admin","org1");
+// channel.createChannel("mychannel", "/root/hyperledger/hyperledger-sdk/test/artifacts/channel/mychannel.tx", "Gyan", "org2").then(function (response) {
+//         console.log(response);
+// }).catch(function (error) {
+//         console.log(error);
+// });
 
-        // channel.joinChannel("business", ["localhost:7051","localhost:7056"], "admin", "org1")
-        // 	.then(function(message) {
-        // 		console.log(message);
-        // 	});
+// channel.joinChannel("mychannel", ["localhost:7051", "localhost:7056"], "admin", "org1")
+//         .then(function (message) {
+//                 console.log(message);
+//         }).catch(function (error) {
+//                 console.log(error);
+//         });
 
 //  Chaincode API's
-        //chaincode.installChaincode(["localhost:7051","localhost:7056"], "mycc", "github.com/example_cc","v1", "admin", "org1");
-        // file shoulld be inside config.GOPATH/src/example_cc.go
+//chaincode.installChaincode(["localhost:7051","localhost:7056"], "mycc", "github.com/example_cc","v1", "admin", "org1");
+// file shoulld be inside config.GOPATH/src/example_cc.go
 
-        //chaincode.instantiateChaincode("business", "mycc", "v1", "init", ["a","100","b","200"],"admin", "org1");
-        //error: chaincode name issue
+//chaincode.instantiateChaincode("business", "mycc", "v1", "init", ["a","100","b","200"],"admin", "org1");
+//error: chaincode name issue
 
-   //     chaincode.queryChaincode("peer1","mychannel","mycc", ['a'], "query", "anwith", "org1"))
-   
+     //chaincode.queryChaincode("peer1","mychannel","mycc", ['a'], "query", "Gyan", "org1")
 
-    //    chaincode.invokeChaincode(["localhost:7051", "localhost:7056"], "business", "mycc", "move", ["a","b","10"], "anwith", "org1")
-        //Query for a after move
-        //console.log(chaincode.queryChaincode("peer1","mychannel","mycc", ['a'], "query", "admin", "org1"));
+
+//    chaincode.invokeChaincode(["localhost:7051", "localhost:7056"], "business", "mycc", "move", ["a","b","10"], "anwith", "org1")
+//Query for a after move
+//console.log(chaincode.queryChaincode("peer1","mychannel","mycc", ['a'], "query", "admin", "org1"));
 
 //  User API's
 //    sdk.helper.getRegisteredUsers("anwith", 'org1', true).then(function(response) {
@@ -37,3 +45,28 @@ helper.setChain("/root/hyperledger/hyperledger-sdk/test/network-config.json","/r
 // 		}
 // 	});
 
+// user.registerEnrollUser("Gyan","org2")
+// .then(function(response){
+//         console.log(response);
+// }).catch(function(error){
+//         console.log(error);
+// });     
+
+// user.enrollUser("Gyan","TigqtAdQAZii","csr","org2")
+// .then(function(response){
+//         console.log(response);
+// }).catch(function(error){
+//         console.log(error);
+// });
+
+
+// user.revokeUser("Gyan",null, 0, "org2")
+// .then(function(response){
+//         console.log(response);
+// }).catch(function(error){
+//         console.log(error);
+// });
+
+
+
+//chain.getChainInfo("peer1", "Gyan", "org2")
